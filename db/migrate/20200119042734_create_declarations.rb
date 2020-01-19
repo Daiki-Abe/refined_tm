3629c9db10f6193f4f995b9224684f0645c325c5
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-01-19 14:50:51 +0900

    change mysql, migrate

--- db/migrate/20200119042734_create_declarations.rb
+++ db/migrate/20200119042734_create_declarations.rb
@@ -1,11 +1,11 @@
 class CreateDeclarations < ActiveRecord::Migration[5.2]
   def change
     create_table :declarations do |t|
-      t.string :what
-      t.string :why
-      t.string :time
+      t.string :what, null: false
+      t.string :why, null: false
+      t.string :time, null: false
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
