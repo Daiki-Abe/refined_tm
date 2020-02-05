197d4517402b02e479697fea585da684914c6332
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-02-05 20:57:22 +0900

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  # この記述がないと、ルーティングエラーになる。具体的には[GET]メソッドを参照してしまう。 
+
   root "declarations#index"
   resources :declarations do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
