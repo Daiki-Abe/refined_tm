75f239f18d3128b9a567a0db8ce64a78b8857689
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-02-11 15:29:31 +0900

    add factorybot

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  
+  factory :user do
+    name {"abe"}
+    email {"beti@gamil.com"}
+    encrypted_password {"00000000"}
+  end
+
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+describe User do
+  describe '#new' do
+    it "nameカラムに入力なしで、ユーザーの登録ができるか" do
+    end
+  end
+end
