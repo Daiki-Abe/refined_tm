40eccc4306c457079905506236464107679fadce
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-02-11 19:23:24 +0900

    add expect

---
+++ spec/models/declaration_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+describe Declaration do
+  describe '#create' do
+    it "全てのカラムに入力があれば、記録登録ができる" do
+      user = create(:user)
+      time = user.declarations.build(what: "勉強", why: "エンジニアになるため", time: "３時間", score: "03:00:00", user_id: 1, start: "15:00", end: "17:00")
+      expect(time).to be_valid
+    end
+
+    it "whatカラムに入力がないと、記録登録ができない" do
+      time = build(:declaration, what: "", end: "17:00")
+      time.valid?
+      expect(time.errors[:what]).to include("を入力してください")
+    end
+
+    it "whenカラムに入力がないと、記録登録ができない" do
+      time = build(:declaration, why: "", end: "17:00")
+      time.valid?
+      expect(time.errors[:why]).to include("を入力してください")
+    end
+
+    it "timeカラムに入力がないと、記録登録ができない" do
+      time = build(:declaration, time: "", end: "17:00")
+      time.valid?
+      expect(time.errors[:time]).to include("を入力してください")
+    end
+
+    it "scoreカラムに入力がないと、記録登録ができない" do
+      time = build(:declaration, score: "", end: "17:00")
+      time.valid?
+      expect(time.errors[:score]).to include("を入力してください")
+    end
+
+    it "user_idカラムに入力がないと、記録登録ができない" do
+      time = build(:declaration, user_id: "", end: "17:00")
+      time.valid?
+      expect(time.errors[:user_id]).to include("を入力してください")
+    end
+
+    it "startカラムに入力がないと、記録登録ができない" do
+      time = build(:declaration, start: "", end: "17:00")
+      time.valid?
+      expect(time.errors[:start]).to include("を入力してください")
+    end
+
+    it "endカラムに入力がないと、記録登録ができない" do
+      time = build(:declaration)
+      time.valid?
+      expect(time.errors[:end]).to include("を入力してください")
+    end
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,35 +7,52 @@ describe User do
       expect(user).to be_valid
     end
 
-    it "nameカラムに入力なしで、ユーザーの登録がでない" do
+    it "nameカラムに入力なしで、ユーザー登録がでない" do
       user = build(:user, name: "")
       user.valid?
       expect(user.errors[:name]).to include("を入力してください")
     end
 
-    it "emailカラムに入力なしで、ユーザーの登録ができない" do
+    it "emailカラムに入力なしで、ユーザー登録ができない" do
       user = build(:user, email: "")
       user.valid?
       expect(user.errors[:email]).to include("を入力してください")
     end
 
-    it "passwordカラムに入力なしで、ユーザーの登録ができない" do
+    it "passwordカラムに入力なしで、ユーザー登録ができない" do
       user = build(:user, password: "")
       user.valid?
       expect(user.errors[:password]).to include("を入力してください")
     end
 
-    it "重複したメールアドレスは登録できない" do
+    it "重複したメールアドレスはユーザー登録ができない" do
       user = create(:user)
       another_user = build(:user)
       another_user.valid?
       expect(another_user.errors[:email]).to include("はすでに存在します")
     end
 
-    it "passwordカラムは存在しても、password_confirmationカラムが存在しないと登録できない" do
+    it "passwordカラムは存在しても、password_confirmationカラムが存在しないと、ユーザー登録ができない" do
       user = build(:user, password_confirmation: "")
       user.valid?
       expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
+    end
+
+    it "passwordカラムとpassword_confirmationカラムが同じ値でないと、ユーザー登録ができない" do
+      user = build(:user, password_confirmation: "666666")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
+    end
+
+    it "passwordカラムに5文字以下だと、ユーザー登録ができない" do
+      user = build(:user, password: "55555")
+      user.valid?
+      expect(user.errors[:password]).to include("は6文字以上で入力してください")
+    end
+
+    it "passwordカラムが６文字以上だと、ユーザー登録ができる" do
+      user = build(:user, password: "7777777", password_confirmation: "7777777")
+      expect(user).to be_valid
     end
   end
 end
